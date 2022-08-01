e6c42c24fe482810e248f91d6ab2b061d07f70bd
Thomas <malifarge.thomas9@gmail.com>
2022-08-01 12:00:47 +0200

    step3 end

--- src/App.js
+++ src/App.js
@@ -51,24 +51,26 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmailChange}/>
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" onChange={handlePasswordChange}/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={handleRememberme}/>
-    <label className="form-check-label" for="exampleCheck1">Remember me</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
+    <>
+    {isSubmited ? <div><p>Welcom <span>{email}</span> </p> </div> : <div className="App">
+    <h1>Login</h1>
+    <form onSubmit={handleSubmit}>
+<div className="mb-3">
+  <label for="exampleInputEmail1" className="form-label">Email address</label>
+  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmailChange}/>
+</div>
+<div className="mb-3">
+  <label for="exampleInputPassword1" className="form-label">Password</label>
+  <input type="password" className="form-control" id="exampleInputPassword1" onChange={handlePasswordChange}/>
+</div>
+<div className="mb-3 form-check">
+  <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={handleRememberme}/>
+  <label className="form-check-label" for="exampleCheck1">Remember me</label>
+</div>
+<button type="submit" className="btn btn-primary">Submit</button>
 </form>
-    </div>
+  </div> }
+  </>
   );
 }
 
