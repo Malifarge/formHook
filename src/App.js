4face575ae0f6dc57934412dce22f36458b1fcf4
Thomas <malifarge.thomas9@gmail.com>
2022-08-01 11:26:57 +0200

    validmail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  // State
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [remember,setRemember] = useState(false)
+  const [emailIsValid,setEmailIsValid] = useState(false)
+  const [passwordIsValid,setPasswordIsValid] = useState(false)
+  const [isSubmited,setIsSubmited] = useState(false)
+
+  const handleEmailChange = (e) =>{
+    setEmail(e.target.value)
+    const regex = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    const mailTest = regex.test(e.target.value)
+    if(mailTest){
+      setEmailIsValid(true)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login</h1>
+      <form>
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmailChange}/>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPassword1" className="form-label">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3 form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
     </div>
   );
 }
